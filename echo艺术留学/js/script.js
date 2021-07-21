003552550b69d674e2614a4faad12a7a996b8212
Luuuooo <79847122+Luuuooo@users.noreply.github.com>
2021-07-21 02:15:50 -0700

    Echo艺术留学

---
+++ echo艺术留学/js/script.js
@@ -0,0 +1,43 @@
+const input = document.querySelector(".finder__input");
+const finder = document.querySelector(".finder");
+const form = document.querySelector("form");
+
+input.addEventListener("focus", () => {
+  finder.classList.add("active");
+});
+
+input.addEventListener("blur", () => {
+  if (input.value.length === 0) {
+    finder.classList.remove("active");
+  }
+});
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  finder.classList.add("processing");
+  finder.classList.remove("active");
+  input.disabled = true;
+  setTimeout(() => {
+    finder.classList.remove("processing");
+    input.disabled = false;
+    if (input.value.length > 0) {
+      finder.classList.add("active");
+    }
+  }, 1000);
+});
+
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+
+$('.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1500, 'easeInOutExpo');
+  return false;
+});
